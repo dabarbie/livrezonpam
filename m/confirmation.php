17da386d8df81970c5d55cf6395ac31f998f905c
yayou <eustache455@gmail.com>
2019-07-02 18:16:28 -0400

    modifikasyon fenet menu_add

--- m/confirmation.php
+++ m/confirmation.php
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr" >
   <head>
     <meta charset="utf-8">
-    <title>Menu add livrezonpam</title>
+    <title>Confirmation livrezonpam</title>
     <link rel="stylesheet" href="rsc/css/master.css">
     <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width" />
     <script type="text/javascript" src="rsc/js/jquery-3.2.1.min.js"> </script>
